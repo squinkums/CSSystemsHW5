c6b7fa7a4974c9192a26b95c7ebe29ac7e7cb608
Liam Goodwin <goodwali@reed.edu>
2020-04-10 19:04:51 +0000

    Preliminary Phase Completed

---
+++ server/handler.hh
@@ -0,0 +1,53 @@
+#ifndef HANDLER_H
+#define HANDLER_H
+#include <iostream>
+#include "stdafx.hh"
+#include <cpprest/asyncrt_utils.h>
+#include "cache.hh"
+
+//#include "../dbms/include/Dbms.h"
+
+
+
+namespace web {
+	namespace http {
+		class http_request;
+	}
+}
+
+using namespace std;
+using namespace web;
+using namespace http;
+using namespace web::http;                  
+using namespace web::http::client;         
+using namespace concurrency::streams;       
+using namespace utility;
+using namespace web::http::experimental::listener;         
+using namespace web::json;
+
+class handler
+{
+    public:
+        handler(utility::string_t url, Cache::size_type maxmem =100,
+            float max_load_factor = 0.75,
+            Evictor* evictor = nullptr,
+            Cache::hash_func hasher = std::hash<key_type>());
+        virtual ~handler();
+
+        pplx::task<void>open(){return m_listener.open();}
+        pplx::task<void>close(){return m_listener.close();}
+
+
+    private:
+        void handle_get(http_request message);
+        void handle_put(http_request message);
+        void handle_post(http_request message);
+        void handle_delete(http_request message);
+        void handle_error(pplx::task<void>& t);
+        void handle_head(http_request message);
+        http::experimental::listener::http_listener m_listener;
+        std::unique_ptr<Cache> my_cache_;
+};
+
+#endif // HANDLER_H
+
